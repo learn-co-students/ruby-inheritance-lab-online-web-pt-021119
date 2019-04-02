e9840cb080ea569335aca8f202b3a85be1c6aca8
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-02 09:19:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,3 @@
 class Student < User
 
-  attr_accessor :first_name, :last_name
-
-  def initialize
-    @knowledge = []
-  end
-
-  def learn(string)
-    @knowledge << string
-  end
-
-  def knowledge
-    @knowledge
-  end
-
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,16 @@ class User
 
   attr_accessor :first_name, :last_name
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 end
